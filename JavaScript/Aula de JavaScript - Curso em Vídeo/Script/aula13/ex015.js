1516d695a463405e8aa934f86e50f7f65300a4e8
Alpha Vylly <65187737+AlphaLawless@users.noreply.github.com>
2021-04-16 16:23:46 -0300

    added new exercise

--- JavaScript/Aula de JavaScript - Curso em Vídeo/Script/aula13/ex015.js
+++ JavaScript/Aula de JavaScript - Curso em Vídeo/Script/aula13/ex015.js
@@ -1,5 +1,13 @@
+//Primeiro Teste Lógico, com a condição no início
 let c = 1
-while (c < 6) {
-  console.log('Tudo OK!')
+while (c <= 6) {
+  console.log(`Passo ${c}`)
   c++
 }
+
+//Segundo Teste Lógico, com a condição no final
+let b = 1
+do {
+  console.log(`Passo ${b}`)
+  b++
+} while (b <= 10)
---
+++ JavaScript/Aula de JavaScript - Curso em Vídeo/Script/aula13/ex016.js
@@ -0,0 +1,9 @@
+//Laço de repetição For (Parte 1)
+for (let c = 1; c <= 10; c++) {
+  console.log(`Passo ${c}`)
+}
+
+//Laço de Repetição For (Parte 2)
+for (let b = 10; b >= 0; b--) {
+  console.log(`Passo ${b}`)
+}
